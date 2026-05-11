53f6f760dce3bb4a7a23b5af0f536c6f696bd907
aaaaaaa34151 <110631639+aaaaaaa34151@users.noreply.github.com>
2026-05-11 12:15:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -701,7 +701,7 @@ elif pagina == "🔐 Painel Admin":
                     ),
                     "specs": st.column_config.TextColumn(
                         "Especialização",
-                        width="medium"
+                        width="small"
                     ),
                     "role": st.column_config.TextColumn(
                         "Função",
